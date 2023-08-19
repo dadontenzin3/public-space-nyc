983ba82087b6a8f5dfd2b5a5cef65ea59db15d0c
dadontenzin3 <dadontenzin1@gmail.com>
2023-08-19 17:16:15 -0400

    wip: added category filter to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
 
 const boroughMapping = {
-    'Brooklyn': 'B',
-    'Queens': 'Q',
-    'Manhattan': 'M',
-    'Bronx': 'X',
-    'Staten island': 'R'
+  'Brooklyn': 'B',
+  'Queens': 'Q',
+  'Manhattan': 'M',
+  'Bronx': 'X',
+  'Staten Island': 'R'
 };
 
 function Home(props) {
   const [eapplyFilter, setEapplyFilter] = useState('');
   const [boroughFilter, setBoroughFilter] = useState('');
+  const [categoryFilter, setCategoryFilter] = useState('');
 
   const getBoroughCode = (userInput) => {
     const userInputLower = userInput.toLowerCase();
-    return boroughMapping[userInputLower] || userInputLower; 
+    return boroughMapping[userInputLower] || userInputLower;
   };
 
   const filteredSpace =
@@ -28,10 +29,14 @@ function Home(props) {
         item.borough &&
         typeof item.borough === 'string' &&
         item.borough.toLowerCase().includes(getBoroughCode(boroughFilter).toLowerCase());
+      const matchCategory =
+        item.typecategory &&
+        typeof item.typecategory === 'string' &&
+        item.typecategory.toLowerCase() === categoryFilter.toLowerCase();
 
-      return (!eapplyFilter || matchEapply) && (!boroughFilter || matchBorough);
+      return (!eapplyFilter || matchEapply) && (!boroughFilter || matchBorough) && (!categoryFilter || matchCategory);
     });
-
+console.log(filteredSpace);
   const handleEapplyFilter = (e) => {
     setEapplyFilter(e.target.value);
   };
@@ -40,18 +45,26 @@ function Home(props) {
     setBoroughFilter(e.target.value);
   };
 
-  const loaded = () => {
-    const uniqueItems = new Set();
+  const handleCategoryFilter = (e) => {
+    setCategoryFilter(e.target.value);
+  };
 
-    filteredSpace.forEach(({ eapply, gisobjid, borough }) => {
+const loaded = () => {
+    const uniqueItems = new Set();
+  
+    filteredSpace.forEach(({ gisobjid }) => {
       uniqueItems.add(gisobjid);
     });
-
+  
     const uniqueItemsArray = Array.from(uniqueItems);
-
+  
+    if (uniqueItemsArray.length === 0) {
+      return <h1>No Results &#128577;</h1>
+    }
+  
     return uniqueItemsArray.map((gisobjid) => {
       const item = filteredSpace.find((entry) => entry.gisobjid === gisobjid);
-
+  
       return (
         <div className="space" key={gisobjid}>
           <h1>{item.eapply}</h1>
@@ -59,7 +72,7 @@ function Home(props) {
       );
     });
   };
-
+  
   const loading = () => {
     return <h1>Loading ...</h1>;
   };
@@ -69,6 +82,8 @@ function Home(props) {
       {boroughName}
     </option>
   ));
+
+  const categoryOptions = ["Buildings/Institutions", "Community Park", "Flagship Park", "Garden", "Historic House Park", "Jointly Operated Playground","Nature Area", "Neighborhood Park", "Parkway", "Playground", "Recreation Field/Courts", "Triangle/Plaza", "Waterfront Facility"];
 
   return (
     <section>
@@ -87,8 +102,22 @@ function Home(props) {
           value={boroughFilter}
           onChange={handleBoroughFilter}
         >
-          <option value="">Drop Down</option>
+          <option value="">Select Borough</option>
           {boroughOptions}
+        </select>
+      </div>
+      <div>
+        <label>Category:</label>
+        <select
+          value={categoryFilter}
+          onChange={handleCategoryFilter}
+        >
+          <option value="">Select Category</option>
+          {categoryOptions.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </div>
       <div>{filteredSpace ? loaded() : loading()}</div>
